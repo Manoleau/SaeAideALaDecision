b372e62a077e73b524aa283979c10e5c617fddf0
Emmanuel Ardoin <emmanuel.ardoin@ypok.com>
2025-06-25 12:06:47 +0200

    d

--- src/main/java/Interface/ImageAccueil.java
+++ src/main/java/Interface/ImageAccueil.java
@@ -3,7 +3,7 @@ package Interface;
 import javax.swing.*;
 
 public enum ImageAccueil {
-	MEDECIN("C:\\Users\\Rafik\\Documents\\SAE\\sae_java_outil_aide_a_la_decision\\Graphe\\src\\img\\doctor.png");
+	MEDECIN("src/img/doctor.png");
 	private ImageIcon imageIcon;
 	ImageAccueil(String imagePath){
 	imageIcon = new ImageIcon(imagePath);
